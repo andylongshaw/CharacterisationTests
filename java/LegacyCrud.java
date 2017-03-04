e018f8017040471fbfb6da071774f3c3e43d22e4
andylongshaw <andy@blueskyline.com>
2017-03-04 19:55:59 +0000

    Initial legacy Java class and gitignore for Java

---
+++ java/LegacyCrud.java
@@ -0,0 +1,8 @@
+public class LegacyCrud
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Cruddy old output");
+    System.out.println("Some more stuff incluing numbers and punctuation 47.9, 28, 78871");
+  }
+}
