a0969ef08ad2eeaf22271eab55dee7718d119404
andylongshaw <andy@blueskyline.com>
2017-03-04 21:30:52 +0000

    pulled canned legacy file into test as expected

--- java/CharacterisationTestForLegacyCrud.java
+++ java/CharacterisationTestForLegacyCrud.java
@@ -1,14 +1,28 @@
 package com.blueskyline.characterisationtests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class CharacterisationTestForLegacyCrud
 {
   @Test
   public void WeHaventBrokenAnything()
   {
-    String cannedOutput = "Wibble";
+    String cannedOutput = "";
+    try
+    {
+      cannedOutput = new String(Files.readAllBytes(Paths.get("legacy_output.txt")));
+    }
+    catch (IOException ex)
+    {
+      fail("Could not load canned legacy output file");
+    }
+
     String capturedOutput = "Wobble";
 
     assertEquals(cannedOutput, capturedOutput);
