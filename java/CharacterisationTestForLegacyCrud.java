471143f9bea17c79f24514ea5469b9030fe9c9d3
andylongshaw <andy@blueskyline.com>
2017-03-04 20:51:22 +0000

    Added empty test class and junit support in makefile

---
+++ java/CharacterisationTestForLegacyCrud.java
@@ -0,0 +1,13 @@
+package com.blueskyline.characterisationtests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CharacterisationTestForLegacyCrud
+{
+  @Test
+  public void WeHaventBrokenAnything()
+  {
+
+  }
+}
